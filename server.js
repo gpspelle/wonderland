1306e7066140ea72f710272576b49e9b515c4f22
Gabriel Pellegrino <gabriel.pellegrino@student-cs.fr>
2021-01-21 11:10:00 +0100

    feat(await): await server call

--- server.js
+++ server.js
@@ -24,16 +24,17 @@ app.post('/api/world', (req, res) => {
 });
 
 app.post('/api/name-to-ticker', (req, res) => {
-  axios.get(`https://www.googleapis.com/customsearch/v1?key=${process.env.googleSearchAPIKey}&cx=${process.env.googleSearchEngineCx}&q=yahoo+finance+${req.body.name}&num=1`)
-    .then(response => {
-      const url = JSON.stringify(response.data.items[0].link);
+  (async () => { 
+    try {
+      const search = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${process.env.googleSearchAPIKey}&cx=${process.env.googleSearchEngineCx}&q=yahoo+finance+${req.body.name}&num=1`);
+      const url = JSON.stringify(search.data.items[0].link);
       const parts = url.split('/');
       const ticker = parts[parts.length - 2]; // -2 because the url ends in / (trailing slash)
       res.send(`Website: ${url}.\nTicker: ${ticker}`);
-    })
-    .catch(error => {
+    } catch (error) {
       console.log(error);
-    });
+    }
+  })();
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
